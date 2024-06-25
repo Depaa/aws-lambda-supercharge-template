64c063d85debf91809f64ca9d0d9d5b2d874b026
Matteo Depascale <depascale.matteo@gmail.com>
2024-06-25 10:38:13 +0200

    feat: add pre-push hook

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,12 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
